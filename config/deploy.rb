3afa67ce8812aae5202087993f4ba2fa7943198b
Lenilson Júnior <me@lenilson.codes>
2018-01-08 12:19:40 -0300

    :wrench: Linked files

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,5 @@
 set :repo_url, "git@github.com:lenilsonjr/Blog.git"
 set :use_sudo, false
 set :keep_releases, 3
+
+set :linked_dirs, %w{ anchor/config/app.php anchor/config/db.php anchor/config/session.php }
