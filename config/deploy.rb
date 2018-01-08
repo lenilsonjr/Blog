aa5b877228d6a60ae43156dbb4968293d6f50cfb
Lenilson Júnior <me@lenilson.codes>
2018-01-08 12:28:10 -0300

    :wrench: Composer in linked files

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@ set :repo_url, "git@github.com:lenilsonjr/Blog.git"
 set :use_sudo, false
 set :keep_releases, 3
 
-set :linked_files, %w{ anchor/config/app.php anchor/config/db.php anchor/config/session.php }
+set :linked_files, %w{ anchor/config/app.php anchor/config/db.php anchor/config/session.php composer.json }
 
 namespace :deploy do
   
