3d83a23488398d5241f1b3ab7808ac00942bb315
Lenilson Júnior <me@lenilson.codes>
2018-01-08 12:22:22 -0300

    :wrench: Composer task

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,3 +3,20 @@ set :use_sudo, false
 set :keep_releases, 3
 
 set :linked_files, %w{ anchor/config/app.php anchor/config/db.php anchor/config/session.php }
+
+namespace :deploy do
+  
+  desc "Update composer dependencies"
+  task :composer_install do
+    on roles(:web) do
+
+      within release_path do
+        info "Running composer"
+        execute 'composer', 'install', '--no-dev', '--optimize-autoloader'
+      end
+
+    end
+  end
+
+  after :updated, "deploy:composer_install"
+end  
